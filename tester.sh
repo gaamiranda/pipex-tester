0268ad5f07a4da9f6006c4860c1a4bc577a32ee5
utm <utm@gmail.com>
2024-08-02 15:06:20 +0100

    here_doc tests, small fixes

--- tester.sh
+++ tester.sh
@@ -259,7 +259,7 @@ bonus() {
 			printf "\t\t${BLUE}TEST 2: ${GREEN}OK${RST}\n"
 		else
 			printf "\t\t${BLUE}TEST 2: ${RED}KO${RST}\n"
-			printf "mandatory TEST 2: input -> ./pipex in/in_1.txt cat grep odio grep -E a$ wc -l out/outfile_user.txt\n" >> errors/errors_log.txt
+			printf "Bonus TEST 2: input -> ./pipex in/in_1.txt cat grep odio grep -E a$ wc -l out/outfile_user.txt\n" >> errors/errors_log.txt
 		fi
 	elif [ $VALGRIND_CODE -eq 1 ]; then
 		printf "\t\t${BLUE}TEST 2: ${YELLOW}KO->memory leaks ${RST}\n"
@@ -282,7 +282,7 @@ bonus() {
 			printf "\t\t${BLUE}TEST 3: ${GREEN}OK${RST}\n"
 		else
 			printf "\t\t${BLUE}TEST 3: ${RED}KO${RST}\n"
-			printf "mandatory TEST 3: input -> ./pipex in/in_1.txt cat grep -v a grep -E s$ out/outfile_user.txt\n" >> errors/errors_log.txt
+			printf "Bonus TEST 3: input -> ./pipex in/in_1.txt cat grep -v a grep -E s$ out/outfile_user.txt\n" >> errors/errors_log.txt
 		fi
 	elif [ $VALGRIND_CODE -eq 1 ]; then
 		printf "\t\t${BLUE}TEST 3: ${YELLOW}KO->memory leaks ${RST}\n"
@@ -304,7 +304,7 @@ bonus() {
 			printf "\t\t${BLUE}TEST 4: ${GREEN}OK${RST}\n"
 		else
 			printf "\t\t${BLUE}TEST 4: ${RED}KO${RST}\n"
-			printf "mandatory TEST 4(invalid command): input -> ./pipex in/in_1.txt ls -l cat wcc ls cat out/outfile_user.txt\n" >> errors/errors_log.txt
+			printf "Bonus TEST 4(invalid command): input -> ./pipex in/in_1.txt ls -l cat wcc ls cat out/outfile_user.txt\n" >> errors/errors_log.txt
 		fi
 	elif [ $VALGRIND_CODE -eq 1 ]; then
 		printf "\t\t${BLUE}TEST 4: ${YELLOW}KO->memory leaks ${RST}\n"
@@ -326,7 +326,7 @@ bonus() {
 			printf "\t\t${BLUE}TEST 5: ${GREEN}OK${RST}\n"
 		else
 			printf "\t\t${BLUE}TEST 5: ${RED}KO${RST}\n"
-			printf "mandatory TEST 5: input -> ./pipex in/in_awk.txt awk -F, \$2 > 80 {print \$1} grep ^A wc -l out/outfile_user.txt\n" >> errors/errors_log.txt
+			printf "Bonus TEST 5: input -> ./pipex in/in_awk.txt awk -F, \$2 > 80 {print \$1} grep ^A wc -l out/outfile_user.txt\n" >> errors/errors_log.txt
 		fi
 	elif [ $VALGRIND_CODE -eq 1 ]; then
 		printf "\t\t${BLUE}TEST 5: ${YELLOW}KO->memory leaks ${RST}\n"
@@ -337,23 +337,182 @@ bonus() {
 	fi
 }
 
-make -C .. re > /dev/null
+here_doc() {
+	printf "${MAGENTA}here_doc part:\n${RST}"
+	#Test 1
+
+	rm -f out/outfile_shell.txt
+	rm -f out/outfile_user.txt
+	cat << c | wc -l >> out/outfile_shell.txt
+lorem
+ipsum
+nulla manga
+anjo
+abc
+cc
+c
+	valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ../$PIPEX here_doc c cat "wc -l" out/outfile_user.txt > errors/valgrind_log.txt 2>&1 <<-c
+lorem
+ipsum
+nulla manga
+anjo
+abc
+cc
+c
+	if [ ! -e out/outfile_user.txt ]; then
+		printf "\t\t${BLUE}TEST 1: ${RED}KO${RST}-> ${BOLD}couldnt create the output file-> outfile_user.txt${RST}\n"
+		make -C .. fclean > /dev/null
+		exit 1
+	fi
+	check_valgrind
+	VALGRIND_CODE=$?
+	diff --brief out/outfile_shell.txt out/outfile_user.txt > /dev/null 2>&1
+	CODE=$?
+	if [ $VALGRIND_CODE -eq 0 ]; then
+		if [ $CODE -eq 0 ]; then
+			printf "\t\t${BLUE}TEST 1: ${GREEN}OK${RST}\n"
+		else
+			printf "\t\t${BLUE}TEST 1: ${RED}KO${RST}\n"
+			printf "${BOLD}If you failed one test in here_doc the others will fail as well since you need to append the results to the outfile${RST}\n${RED}ABORTING${RST}\n"
+			printf "here_doc TEST 1: input -> ./pipex  here_doc c cat wc -l out/outfile_user.txt, check what words I sent to pipex in line 354 of the script!\n" >> errors/errors_log.txt
+			exit 1
+		fi
+	elif [ $VALGRIND_CODE -eq 1 ]; then
+		printf "\t\t${BLUE}TEST 1: ${YELLOW}KO->memory leaks ${RST}\n"
+		printf "here_doc TEST 1 leaks above\n" >> errors/errors_log.txt
+	elif [ $VALGRIND_CODE -eq 2 ]; then
+		printf "\t\t${BLUE}TEST 1: ${YELLOW}KO->open fds ${RST}\n"
+		printf "here_doc TEST 1 leaks above\n" >> errors/errors_log.txt
+	fi
+	#Test 2
+
+	cat << EOF | wc -l >> out/outfile_shell.txt
+lorem
+ipsum
+nulla manga
+anjo
+abc
+cc
+EOF
+	valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ../$PIPEX here_doc EOF cat "wc -l" out/outfile_user.txt > errors/valgrind_log.txt 2>&1 <<-EOF
+lorem
+ipsum
+nulla manga
+anjo
+abc
+cc
+EOF
+	check_valgrind
+	VALGRIND_CODE=$?
+	diff --brief out/outfile_shell.txt out/outfile_user.txt > /dev/null 2>&1
+	CODE=$?
+	if [ $VALGRIND_CODE -eq 0 ]; then
+		if [ $CODE -eq 0 ]; then
+			printf "\t\t${BLUE}TEST 2: ${GREEN}OK${RST}\n"
+		else
+			printf "\t\t${BLUE}TEST 2: ${RED}KO${RST}\n"
+			printf "here_doc TEST 2: input -> ./pipex  here_doc EOF cat wc -l out/outfile_user.txt, check what words I sent to pipex in line 387 of the script!\n" >> errors/errors_log.txt
+			printf "${BOLD}If you failed one test in here_doc the others will fail as well since you need to append the results to the outfile${RST}\n${RED}ABORTING${RST}\n"
+			exit 1
+		fi
+	elif [ $VALGRIND_CODE -eq 1 ]; then
+		printf "\t\t${BLUE}TEST 2: ${YELLOW}KO->memory leaks ${RST}\n"
+		printf "here_doc TEST 2 leaks above\n" >> errors/errors_log.txt
+	elif [ $VALGRIND_CODE -eq 2 ]; then
+		printf "\t\t${BLUE}TEST 2: ${YELLOW}KO->open fds ${RST}\n"
+		printf "here_doc TEST 2 leaks above\n" >> errors/errors_log.txt
+	fi
+	#Test 3
+
+	cat << EOF | wc >> out/outfile_shell.txt
+EOF
+	valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ../$PIPEX here_doc EOF cat wc out/outfile_user.txt > errors/valgrind_log.txt 2>&1 <<-EOF
+EOF
+	check_valgrind
+	VALGRIND_CODE=$?
+	diff --brief out/outfile_shell.txt out/outfile_user.txt > /dev/null 2>&1
+	CODE=$?
+	if [ $VALGRIND_CODE -eq 0 ]; then
+		if [ $CODE -eq 0 ]; then
+			printf "\t\t${BLUE}TEST 3: ${GREEN}OK${RST}\n"
+		else
+			printf "\t\t${BLUE}TEST 3: ${RED}KO${RST}\n"
+			printf "here_doc TEST 3: input -> ./pipex  here_doc EOF cat wc out/outfile_user.txt, didnt send any words to pipex, just an EOF, line 410 of the script!\n" >> errors/errors_log.txt
+			printf "${BOLD}If you failed one test in here_doc the others will fail as well since you need to append the results to the outfile${RST}\n${RED}ABORTING${RST}\n"
+			exit 1
+		fi
+	elif [ $VALGRIND_CODE -eq 1 ]; then
+		printf "\t\t${BLUE}TEST 3: ${YELLOW}KO->memory leaks ${RST}\n"
+		printf "here_doc TEST 3 leaks above\n" >> errors/errors_log.txt
+	elif [ $VALGRIND_CODE -eq 2 ]; then
+		printf "\t\t${BLUE}TEST 3: ${YELLOW}KO->open fds ${RST}\n"
+		printf "here_doc TEST 3 leaks above\n" >> errors/errors_log.txt
+	fi
+	#Test 4
+
+	grep "a" << a | cat >> out/outfile_shell.txt
+a doll
+will it work
+maybe a dolphin
+aa
+a
+	valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes ../$PIPEX here_doc a "grep "a"" cat out/outfile_user.txt > errors/valgrind_log.txt 2>&1 <<-a
+a doll
+will it work
+maybe a dolphin
+aa
+a
+	check_valgrind
+	VALGRIND_CODE=$?
+	diff --brief out/outfile_shell.txt out/outfile_user.txt > /dev/null 2>&1
+	CODE=$?
+	if [ $VALGRIND_CODE -eq 0 ]; then
+		if [ $CODE -eq 0 ]; then
+			printf "\t\t${BLUE}TEST 4: ${GREEN}OK${RST}\n"
+		else
+			printf "\t\t${BLUE}TEST 4: ${RED}KO${RST}\n"
+			printf "here_doc TEST 4: input -> ./pipex here_doc a "grep "a"" cat out/outfile_user.txt, line 459 of the script if you wanna check the words!\n" >> errors/errors_log.txt
+			printf "${BOLD}If you failed one test in here_doc the others will fail as well since you need to append the results to the outfile${RST}\n${RED}ABORTING${RST}\n"
+			exit 1
+		fi
+	elif [ $VALGRIND_CODE -eq 1 ]; then
+		printf "\t\t${BLUE}TEST 4: ${YELLOW}KO->memory leaks ${RST}\n"
+		printf "here_doc TEST 4 leaks above\n" >> errors/errors_log.txt
+	elif [ $VALGRIND_CODE -eq 2 ]; then
+		printf "\t\t${BLUE}TEST 4: ${YELLOW}KO->open fds ${RST}\n"
+		printf "here_doc TEST 4 leaks above\n" >> errors/errors_log.txt
+	fi
+
+}
+
+
 printf "Errors:\n" > errors/errors_log.txt
 printf "\t\t\t${GREEN}Welcome to pipex-tester by gaamiranda${RST}\n"
 printf "\t\t\t${GREEN}If it helps you give a star :)${RST}\n"
 
 if [ -z "$1" ]; then
-	printf "${RED}main bonus${RST}"
+	make -C .. re > /dev/null
+	mandatory
+	bonus
+	here_doc
 elif [ "$1" == "m" ]; then
+	make -C .. re > /dev/null
 	mandatory
 elif [ "$1" == "b" ]; then
+	make -C .. re > /dev/null
 	mandatory
 	bonus
 elif [ "$1" == "h" ]; then
-	echo "here_doc"
+	make -C .. re > /dev/null
+	here_doc
 else
 	echo "Invalid option"
+	printf "${BOLD} ./pipex ->   mandatory bonus here_doc tests${RST}\n"
+	printf "${BOLD} ./pipex m -> mandatory tests${RST}\n"
+	printf "${BOLD} ./pipex b -> mandatory and multiple pipes bonus tests${RST}\n"
+	printf "${BOLD} ./pipex h -> here_doc tests${RST}\n"
 	exit
 fi
 printf "\n\n${BOLD}You can find error logs in errors/errors_log.txt${RST}\n"
+printf "\n\n${RED}If you have open fds I suggest you also do the tests by yourself as this implementation to check open fds isnt perfect!${RST}\n"
 make -C .. fclean > /dev/null
